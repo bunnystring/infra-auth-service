a0cf743fe8f4daa961353c61ef40918fd850267b
bunnystring <ziknaf_548@hotmail.com>
2025-10-25 08:21:31 -0500

    Add infra-auth-service with project basic server configuration

---
+++ src/main/java/infragest/infra_auth_service/InfraAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package infragest.infra_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/infragest/infra_auth_service/InfraAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package infragest.infra_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
